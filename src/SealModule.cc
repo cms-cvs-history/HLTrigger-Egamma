02db83f832b2b46fcad16eb51e1de6d2f00c801b
cmsbuild <>
2007-02-26 18:26:00 +0000

    This commit was manufactured by cvs2git to create tag 'HLTrigger-Egamma- CMSSW_1_3_0_pre5'.
    Sprout from master 2007-02-26 18:25:59 UTC Monica Vazquez Acosta <sha1-d6d4e0778bbf59996987404c0bdf8ea90d29fdba@cern.ch> 'use L1emulator HLTrigger/Configuration and use RecoEcal/Configuration for ecalclustering'
    Cherrypick from master 2007-02-01 15:18:46 UTC Monica Vazquez Acosta <sha1-d6d4e0778bbf59996987404c0bdf8ea90d29fdba@cern.ch> 'Added new L1-SC Filter for Relaxed candidates as two L1 collections (isol, non-isol) need to be used':
        src/SealModule.cc
    Delete:
        data/hltZeePMDocaFilter.cfi
        data/hltZeePMMassFilter.cfi
        interface/HLTPMDocaFilter.h
        interface/HLTPMMassFilter.h
        src/HLTPMDocaFilter.cc
        src/HLTPMMassFilter.cc

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -11,9 +11,6 @@
 #include "HLTrigger/Egamma/interface/HLTElectronTrackIsolFilter.h"
 #include "HLTrigger/Egamma/interface/HLTElectronPixelMatchFilter.h"
 #include "HLTrigger/Egamma/interface/HLTElectronEoverpFilter.h"
-#include "HLTrigger/Egamma/interface/HLTPMMassFilter.h"
-#include "HLTrigger/Egamma/interface/HLTPMDocaFilter.h"
-
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HLTEgammaL1MatchFilter);
@@ -26,5 +23,3 @@ DEFINE_ANOTHER_FWK_MODULE(HLTPhotonTrackIsolFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTElectronTrackIsolFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTElectronPixelMatchFilter);
 DEFINE_ANOTHER_FWK_MODULE(HLTElectronEoverpFilter);
-DEFINE_ANOTHER_FWK_MODULE(HLTPMMassFilter);
-DEFINE_ANOTHER_FWK_MODULE(HLTPMDocaFilter);
